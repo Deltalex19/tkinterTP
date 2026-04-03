1f2bfa41255ed8637eb3d4b920706cea43882bd3
Deltalex19 <alexis.de@lycee-de-beauvoir.education>
2026-04-03 09:38:46 +0200

    test fenetre

--- alexis_force_manege.py
+++ alexis_force_manege.py
@@ -1,7 +1,25 @@
 import tkinter as tk
-
-print("test")
+from idlelib.outwin import file_line_pats
+from tkinter.constants import BOTTOM
+from tkinter import messagebox
+def renvoyer_age():
+    messagebox.showinfo("Bonjour", "Bonjour")
 
 fenetre = tk.Tk()
-fenetre.title("Mon application")
+fenetre.geometry("450x300")
+fenetre.title("TP Manège Parc d'Attraction - BTS CIEL")
+labeltitre = tk.Label(fenetre, text="Manège du parc - Force1/Force2", font=("Arial", 18), bg='mintcream', fg='navy')
+labeltitre.pack(fill='both',padx=0,pady=0,side='top')
+labeldesc = tk.Label(fenetre, text="Entrez votre âge et votre taille pour savoir à quelle force vous pouvez faire le manège.", wraplength=350, font=("Arial", 12))
+labeldesc.pack(fill='both',padx=0,pady=0)
+labelage = tk.Label(fenetre, text="Entrez votre âge :", wraplength=250, font=("Arial", 10))
+labelage.pack(fill='both',padx=0,pady=3)
+entryage =tk.Entry(fenetre)
+entryage.pack(fill='both',padx=130,pady=3)
+labeltaille = tk.Label(fenetre, text="Entrez votre taille (en m,exemple: 1.73) :", wraplength=300, font=("Arial", 10))
+labeltaille.pack(fill='both',padx=0,pady=3)
+entrytaille =tk.Entry(fenetre)
+entrytaille.pack(fill='both',padx=130,pady=3)
+buttonenv = tk.Button(fenetre, text="Envoyer", bg='white', command=renvoyer_age)
+buttonenv.pack(fill='both',padx=185,pady=9)
 fenetre.mainloop()
